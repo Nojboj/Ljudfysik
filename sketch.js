abfbc2b9b5c9655a1f67bb140f76d6c7fec76edc
Nojboj <55584885+Nojboj@users.noreply.github.com>
2021-09-27 19:17:24 +0200

    oompa loompa

--- sketch.js
+++ sketch.js
@@ -1,22 +1,36 @@
 let mic, fft;
+let threshold = 0.3;
 
+//för hps, hitta alla höga frekvenser, hitta kvot/multipel mellan dem --> gör algoritm för hps
 function getLoudestFrequency() {
   var nyquist = sampleRate() / 2; // 22050
   var spectrum = fft.analyze(); // array of amplitudes in bins
   var numberOfBins = spectrum.length;
   var maxAmp = 0;
   var largestBin;
-
+  
   for (var i = 0; i < numberOfBins; i++) {
-      var thisAmp = spectrum[i]; // amplitude of current bin
+    var thisAmp = spectrum[i]; // amplitude of current bin     
       if (thisAmp > maxAmp) {
           maxAmp = thisAmp;
           largestBin = i;
-      }
+      }      
   }
 
+
   var loudestFreq = largestBin * (nyquist / numberOfBins);
-  return loudestFreq;
+  return loudestFreq, maxAmp;
+}
+
+function removeNoise(Spectra, maxAmp){
+  for (var i = 0; i < Spectra.length; i++){
+    Spectra[i] = Spectra[i]/maxAmp;
+    
+    if(Spectra[i] < threshold){
+      Spectra[i] = 0;
+    }
+  }
+  return Spectra;
 }
 
 function setup() {
@@ -33,13 +47,15 @@ function draw() {
   background(200);
 
   let spectrum = fft.analyze();
+  let highestFreq;
+  let maxA;
 
-  let frequency = getLoudestFrequency();
-  console.log(frequency);
-
+  highestFreq, maxA = getLoudestFrequency();
+  spectrum = removeNoise(spectrum, maxA);
+  
   beginShape();
   for (i = 0; i < spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0));
+    vertex(i, map(spectrum[i], 0, 1, height, 0));
   }
   endShape();
 }
